be341b9c82523204ee754073ea26846ddadc8448
Nzomaigit config --global user.email <444200503@student.ksu.edu.sa>
2026-03-04 03:34:44 +0300

    routes

--- server/routes/insightRoutes.js
+++ server/routes/insightRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getInsights } from '../controllers/insightController.js';
+import { getInsights, getChildChart, getParentChart } from '../controllers/insightController.js';
 
 const router = express.Router();
 
-router.get('/:childId', getInsights);
-
+// المسارات الجديدة للرسوم البيانية
 router.get('/child-chart/:childId', getChildChart);
 router.get('/parent-chart/:parentId', getParentChart);
+
+// المسار القديم الخاص بالرسائل الذكية
+router.get('/:childId', getInsights);
 
 export default router;
